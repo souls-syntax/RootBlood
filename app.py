dd33694f4428fcb9e1580de73d0861975e095a19
souls-syntax <souls.syntax@gmail.com>
2025-11-11 22:21:52 +0530

    feat: Create user sesstion complete

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 import subprocess
-import threading
+import thread
 import time
 from datetime import datetime, timezone, timedelta
+import logging as log
 
 import docker
 from flask import Flask, jsonify, request
@@ -12,22 +13,43 @@ from flask_sqlalchemy import SQLAlchemy
 # 1. Configuration
 #-------------------------------------
 # All the settings are defined here for easy management
+# ------------------------------------
 
-class Config:
     
-    # Path to the shared "World" directory on host machine
-    BASE_PLAYGROUND_PATH = 'srv/playground'
-    # Name of the docker image we will be using.
-    DOCKER_IMAGE_NAME = 'chaospine:1.0.0'
-    # Path for helper script to make os users
-    UTILITY_USER_SCRIPT = 'path/to/user/script'
-    #database configuration
-    SQLALCHEMY_DB_PATH = 'sqlit:///memory.db'
-    SQLALCHEMY_TRACK_MODIFICATION = False
-    #Garbage collector configuration.
-    SESSION_IDLE_TTL_SECONDS = 72 * 3600
-    SESSION_ACTIVITY_CHECK_INTERVAL_SECONDS = 10 * 60
+# Path to the shared "World" directory on host machine
+BASE_PLAYGROUND_PATH = 'srv/playground'
 
+# Name of the docker image we will be using.
+DOCKER_IMAGE_NAME = 'chaospine:1.0.0'
+
+# Path for helper script to make os users
+UTILITY_USER_SCRIPT = 'path/to/user/script'
+
+# database configuration
+SQLALCHEMY_DB_PATH = 'sqlit:///memory.db'
+SQLALCHEMY_TRACK_MODIFICATION = False
+
+# Garbage collector configuration.
+SESSION_IDLE_TTL_SECONDS = 72 * 3600
+SESSION_ACTIVITY_CHECK_INTERVAL_SECONDS = 10 * 60
+
+# Docker configuration
+DOCKER = docker.from_env()
+
+# Logging configuration
+log.basicConfig(
+    level=logging.info,  # or DEBUG, WARNING, ERROR, CRITICAL
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename='app.log',  # optional: writes logs to a file
+    filemode='a'         # append mode
+)
+
+# Port configuration
+PORT_BEING_USED = '7681/tcp'
+level=logging.INFO,  # or DEBUG, WARNING, ERROR, CRITICAL
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename='app.log',  # optional: writes logs to a file
+    filemode='a'         # append mode
 # ----------------------------------------------
 # 2. DATABASE MODEL
 # ----------------------------------------------
@@ -39,6 +61,7 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    userhash = db.Column(db.String(80), unique=True, nullable=False)
 
 class Project(db.Model):
     """REPRESENT THE PUBLIC HOME DIRECTORY IN THE 'WORLD'..."""
@@ -47,13 +70,22 @@ class Project(db.Model):
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     owner = db.relationship('User', backref=db.backref('directories', lazy=True))
 
-class Contribution(db.Model):
-    # Tracks the active contribution workspace
-    id = db.Column(db.String(32), primary_key=True)
-    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
-    contributor_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    volume_name = db.Column(db.String(256), unique=True, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+#----------------------------------------------
+# We are discarding the contributer model for the better user experience by implementing the specific technology `git`.
+#----------------------------------------------
+
+# class Contribution(db.Model):
+#     # Tracks the active contribution workspace
+#     id = db.Column(db.String(32), primary_key=True)
+#     project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
+#     contributor_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+#     volume_name = db.Column(db.String(256), unique=True, nullable=False)
+#     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+#
+
+#----------------------------------------------
+# Currently no implementing the garbage collector for completion of prototype model.
+#----------------------------------------------
 
 class ActiveSession(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -61,4 +93,45 @@ class ActiveSession(db.Model):
     container_name = db.Column(db.String(128), unique=True, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     last_active = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+
+#---------------------------------------------
+# Defining user session
+#---------------------------------------------
+
+class UserManager:
+
+    def __init__(self, userhash):
+        self.userhash = userhash
     
+    def starts_user_session(self):
+        container_name = f"rootblood_session_{self.userhash}"
+        volume_name = f"home_{self.userhash}"
+
+        try:
+            container = DOCKER.containers.get(container_name)
+
+            if container.status != 'running':
+                    log.info(f"Found stopped container for {self.userhash}. Starting it....")
+                    container.start()
+            else:
+                    log.info(f"Container for {self.userhash} is already running....")
+        except docker.errors.NotFound:
+            log.info(f"No container found for {self.username}. Creating a new one...")
+            DOCKER.containers.run(
+                DOCKER_IMAGE_NAME,
+                detach=True,
+                name=container_name,
+                volumes={volume_name:{'bind':f'/home/{userhash}', 'mode':'rw'},
+                         BASE_PLAYGROUND_PATH:{'bind':'/global/','mode':'rw'}},
+                ports={PORT_BEING_USED: None},
+                working_dir=f'/home/{self.userhash}',
+                stdin_open=True,
+                tty=True
+            )
+
+        # TODO: Will need to write code for tracking the container here.
+        # TODO: Will need to write the tarck_session(container) function
+
+        host_port = container.ports[PORT_BEING_USED][0]['HostPort']
+        return {"session_url":f"http://127.0.0.1:{host_port}", "container_name":container_name}
+
